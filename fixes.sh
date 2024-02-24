c65dd4e320a1828241a2673400d5aea8c2e3c41e
jayz1212 <160797500+jayz1212@users.noreply.github.com>
2024-02-25 03:26:40 +0800

    s

---
+++ fixes.sh
@@ -0,0 +1,23 @@
+## Cam fix for LG G6 and delete some line cause im stupid.
+cd frameworks/base/
+git fetch https://github.com/xc112lg/android_frameworks_base-1.git patch-17
+git cherry-pick a245af744209ccb9cb6ad6981f181fa8a9ba65c5
+cd ../../
+
+# Mixer: adjust input volume levels
+cd device/lge/g6-common
+git fetch https://github.com/LG-G6/android_device_lge_g6-common.git dev/lineage-19.1
+git cherry-pick b3edeba5ac6500c145fec7222ffc696c9b819af0
+cd ../../../
+
+#some fixes will be push to source fter testingg
+cd device/lge/msm8996-common
+ls
+sleep 1 &&git fetch https://github.com/xc112lg/android_device_lge_msm8996-common.git cd10
+sleep 1 &&git cherry-pick aef6632c220ec671b69ed3564d37f74cca295ce2 
+sleep 1 &&git cherry-pick 7ac9890a15cbf0be818fa00c8374620fb5c737c1
+cd ../../../
+#added crdroid setting 
+wget -N -P device/lge/msm8996-common/overlay/frameworks/base/core/res/res/values/ https://github.com/crdroidandroid/android_frameworks_base/raw/14.0/core/res/res/values/cr_config.xml
+wget -N -P device/lge/msm8996-common/overlay/frameworks/base/packages/SystemUI/res/values/ https://github.com/crdroidandroid/android_frameworks_base/raw/14.0/packages/SystemUI/res/values/cr_config.xml
+
