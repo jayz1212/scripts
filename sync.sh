746fb656a81ee6a34ce5f3485166ce945d24381f
jayz1212 <160797500+jayz1212@users.noreply.github.com>
2024-03-08 07:59:04 +0800

    test

--- sync.sh
+++ sync.sh
@@ -1,59 +1,38 @@
 #!/bin/bash
-# Set default values for device and command
-DEVICE="${1:-all}"  # If no value is provided, default to "all"
-COMMAND="${2:-build}"  # If no value is provided, default to "build"
-DELZIP="${3}"
+# Function to wait for 1 second
+wait_one_second() {
+    sleep 1
+}
+
+# Remove existing build artifacts
+wait_one_second && rm -rf out/target/product/*/*.zip device/lge/msm8996-common
 
 # Update and install ccache
-sudo apt-get update -y
-sudo apt-get install -y apt-utils
-sudo apt-get install -y ccache
-export USE_CCACHE=1
-ccache -M 100G
-export CCACHE_DIR=/tmp/src/android/cc
+wait_one_second && sudo apt-get update -y
+wait_one_second && sudo apt-get install -y --no-install-recommends apt-utils
+wait_one_second && sudo apt-get install -y ccache
+wait_one_second && export USE_CCACHE=1
+wait_one_second && export CCACHE_DIR=${PWD}/cc
+wait_one_second && ccache -M 100G
 echo $CCACHE_DIR
-## Remove existing build artifacts
-if [ "$DELZIP" == "delzip" ]; then
-    rm -rf out/target/product/*/*.zip
-fi
-
-#git clean -fdX
-rm -rf .repo/projects/prebuilts
-rm -rf prebuilts
-#rm -rf .repo/local_manifests
-#rm -rf device/lge/
-#rm -rf kernel/lge/msm8996
-mkdir -p .repo/local_manifests
+echo $CCACHE_EXEC
+rm -rf .repo/local_manifests
+mkdir .repo/local_manifests
 cp scripts/roomservice.xml .repo/local_manifests
-repo sync -c -j16 --force-sync --no-clone-bundle --no-tags --prune
-source scripts/fixes.sh
+repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
 source build/envsetup.sh
+#m clean
+#make clean
+rm out/target/product/*/*.zip
+#source scripts/fixes.sh
 
 
-# Check if command is "clean"
-if [ "$COMMAND" == "clean" ]; then
-    echo "Cleaning..."
-    m clean
-fi
 
-# Check if device is set to "all"
-if [ "$DEVICE" == "all" ]; then
-    echo "Building for all devices..."
-    m installclean
-    lunch lineage_us997-userdebug
-    m -j16 bacon
-    lunch lineage_h870-userdebug
-    m -j16 bacon
-    lunch lineage_h872-userdebug
-    m -j16 bacon
- 
-elif [ "$DEVICE" == "h872" ]; then
-    echo "Building for h872..."
-    lunch lineage_h872-userdebug
-    m -j16 bacon
-else
-    echo "Building for the specified device: $DEVICE..."
-    # Build for the specified device
-    lunch "$DEVICE"
-    m -j16 bacon
-fi
+
+lunch evolution_h872-eng
+m installclean
+m -j$(nproc --all) evolution
+#lunch lineage_us997-userdebug
+#m -j$(nproc --all) bacon
+#lunch lineage_h870-userdebug
+#m -j$(nproc --all) bacon
