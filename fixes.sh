b25df1e70d467dcfa36689e7fbef4f1aadee9117
jayz1212 <160797500+jayz1212@users.noreply.github.com>
2024-03-08 18:26:41 +0800

    test

--- fixes.sh
+++ fixes.sh
@@ -4,7 +4,7 @@ git fetch https://github.com/xc112lg/frameworks_base.git evo
 sleep 1 &&git cherry-pick 39c4fa1501de6346603419427782d406c0dcc05f
 sleep 1 &&git cherry-pick e294b96733fe90f4235ec2abd7e3f2efc2ab2430
 sleep 1 &&git cherry-pick 2400aeaac60582149dde363ca7fdf6718392f977
-sleep 1 &&git cherry-pick 095d9a49fa18ff1ac7e032091f270cd1e7c4a340
+sleep 1 &&git cherry-pick 095d9a49fa18ff1ac7e032091f270cd1e7c4a340 670f5c811a4217d5e311d62677d45a80d1272bd4
 cd ../../
 
 # Settings Fix
