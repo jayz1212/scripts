3191b845fc2a33b6e34d714defaa29fa5521886b
jayz1212 <160797500+jayz1212@users.noreply.github.com>
2024-03-10 10:32:44 +0800

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -5,6 +5,7 @@ wait_one_second() {
 }
 mkdir -p cc
 mkdir -p c
+rm -rf c
 rsync -avu ${PWD}/cc  ${PWD}/c
 #cp -rf ${PWD}/cc  ${PWD}/c
 # Remove existing build artifacts
@@ -18,11 +19,14 @@ wait_one_second && sudo apt-get update -y
 wait_one_second && sudo apt-get install -y --no-install-recommends apt-utils
 wait_one_second && sudo apt-get install -y ccache
 wait_one_second && export USE_CCACHE=1
+ccache -s
 wait_one_second && export CCACHE_DIR=${PWD}/cc
 wait_one_second && ccache -M 100G
+ccache -s
+ccache -s
 echo $CCACHE_DIR
 echo $CCACHE_EXEC
-rsync -avu ${PWD}/c  ${PWD}/cc
+rsync -avu --ignore-existing ${PWD}/c  ${PWD}/cc
 rm -rf .repo/local_manifests
 mkdir .repo/local_manifests
 cp scripts/roomservice.xml .repo/local_manifests
