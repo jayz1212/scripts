38a711b1981604df91e99aa518fb4d5b61d338be
jayz1212 <160797500+jayz1212@users.noreply.github.com>
2024-03-10 09:45:43 +0800

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -5,19 +5,23 @@ wait_one_second() {
 }
 mkdir -p cc
 mkdir -p c
+cp -r ${PWD}/cc  ${PWD}/c
 # Remove existing build artifacts
 wait_one_second && rm -rf out/target/product/*/*.zip device/lge/msm8996-common
 ccache -s
 # Update and install ccache
+sudo apt-get install dialog
+sudo apt-get -y install --reinstall ccache
+
 wait_one_second && sudo apt-get update -y
 wait_one_second && sudo apt-get install -y --no-install-recommends apt-utils
 wait_one_second && sudo apt-get install -y ccache
 wait_one_second && export USE_CCACHE=1
-wait_one_second && export CCACHE_DIR=${PWD}/c
+wait_one_second && export CCACHE_DIR=${PWD}/cc
 wait_one_second && ccache -M 100G
 echo $CCACHE_DIR
 echo $CCACHE_EXEC
-cp -r ${PWD}/cc  ${PWD}/c
+cp -r ${PWD}/c  ${PWD}/cc
 rm -rf .repo/local_manifests
 mkdir .repo/local_manifests
 cp scripts/roomservice.xml .repo/local_manifests
