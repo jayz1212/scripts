895b9efa335d9d5456a652224e5b60f24798f74e
jayz1212 <160797500+jayz1212@users.noreply.github.com>
2024-03-16 11:45:12 +0800

    g

--- sync.sh
+++ sync.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-rm -rf .repo/local_manifests hardware/qcom-caf
+rm -rf .repo/local_manifests
 mkdir .repo/local_manifests
 cp scripts/roomservice.xml .repo/local_manifests
 mv scripts/statix.xml .repo/manifests/snippets
