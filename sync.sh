e2c1dcb754afcd982a2422e8ebb51d8b782274cf
jayz1212 <160797500+jayz1212@users.noreply.github.com>
2024-03-16 09:12:35 +0800

    g

--- sync.sh
+++ sync.sh
@@ -1,38 +1,5 @@
 #!/bin/bash
 # Function to wait for 1 second
-wait_one_second() {
-    sleep 1
-}
-mkdir -p cc
-mkdir -p c
-cp -f cc/ccache.conf c/ccache.conf 
-time ls -1 cc | xargs -I {} -P 5 -n 1 rsync -au cc/{} c/
-#time rsync -au --parallel=8 ${PWD}/cc  ${PWD}/c
-#cp -rf ${PWD}/cc  ${PWD}/c
-# Remove existing build artifacts
-wait_one_second && rm -rf out/target/product/*/*.zip device/lge/msm8996-common
-ccache -s
-# Update and install ccache
-sudo apt-get install dialog
-sudo apt-get -y install --reinstall ccache
-
-wait_one_second && sudo apt-get update -y
-wait_one_second && sudo apt-get install -y --no-install-recommends apt-utils
-wait_one_second && sudo apt-get install -y ccache
-wait_one_second && export USE_CCACHE=1
-ccache -s
-wait_one_second && export CCACHE_DIR=${PWD}/cc
-wait_one_second && ccache -M 100G
-ccache -s
-ccache -s
-ccache -o compression=false
-ccache --show-config | grep compression
-echo $CCACHE_DIR
-echo $CCACHE_EXEC
-time ls -1 c | xargs -I {} -P 5 -n 1 rsync -au c/{} cc/
-cp -f c/ccache.conf cc/ccache.conf 
-ccache -o compression=false
-ccache --show-config | grep compression
 
 rm -rf .repo/local_manifests
 mkdir .repo/local_manifests
