d1add7521dd4685bfec5d11db07c821c1dde7aa0
jayz1212 <160797500+jayz1212@users.noreply.github.com>
2024-03-16 09:27:40 +0800

    t

--- sync.sh
+++ sync.sh
@@ -10,17 +10,17 @@ repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
 source build/envsetup.sh
 #m clean
 #make clean
-ccache -s
+# ccache -s
 
- rm out/target/product/*/*.zip
-# source scripts/fixes.sh
-#source build/envsetup.sh && lunch evolution_h872-eng && (while true; do clear; ccache -s; sleep 60; done) & m -j$(nproc --all) evolution
+#  rm out/target/product/*/*.zip
+# # source scripts/fixes.sh
+# #source build/envsetup.sh && lunch evolution_h872-eng && (while true; do clear; ccache -s; sleep 60; done) & m -j$(nproc --all) evolution
 
-source build/envsetup.sh
-lunch evolution_h872-eng
+# source build/envsetup.sh
+# lunch evolution_h872-eng
 
-m -j$(nproc --all) evolution
-#lunch lineage_us997-userdebug
-#m -j$(nproc --all) bacon
-#lunch lineage_h870-userdebug
-#m -j$(nproc --all) bacon
+# m -j$(nproc --all) evolution
+# #lunch lineage_us997-userdebug
+# #m -j$(nproc --all) bacon
+# #lunch lineage_h870-userdebug
+# #m -j$(nproc --all) bacon
