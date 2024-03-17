e213effa3e9db3fc2af9dcf97e264c0ebd6e2933
jayz1212 <160797500+jayz1212@users.noreply.github.com>
2024-03-17 13:51:57 +0800

    test

--- sync.sh
+++ sync.sh
@@ -4,15 +4,9 @@ mkdir .repo/local_manifests
 cp scripts/roomservice.xml .repo/local_manifests
 mv scripts/statix.xml .repo/manifests/snippets
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
-source build/envsetup.sh
+chmod +x tsync.sh
+. tsync.sh
 #m clean
 #make clean
 
- rm out/target/product/*/*.zip
-# source scripts/fixes.sh
-#source build/envsetup.sh && lunch evolution_h872-eng && (while true; do clear; ccache -s; sleep 60; done) & m -j$(nproc --all) evolution
 
-source build/envsetup.sh
-lunch cipher_h872-eng
-
-m -j$(nproc --all) bacon
