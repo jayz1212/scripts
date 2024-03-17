e054837cfa1892bd6e878f1d67d9b7e959adc604
jayz1212 <160797500+jayz1212@users.noreply.github.com>
2024-03-17 14:15:02 +0800

    test

--- sync.sh
+++ sync.sh
@@ -1,4 +1,27 @@
 #!/bin/bash
+
+# Update and install ccache
+sudo apt-get update -y
+sudo apt-get install -y apt-utils
+sudo apt-get install -y ccache
+export USE_CCACHE=1
+ccache -M 100G
+export CCACHE_DIR=${PWD}/cc
+ccache -s
+ccache --set-config=compression=false
+ccache --show-config | grep compression
+echo $CCACHE_DIR
+
+if [ -z "$(ls -A c)" ]; then
+  echo "Folder c is empty. Skipping the rsync command."
+else
+  # If folder c is not empty, execute the rsync command
+time ls -1 c | xargs -I {} -P 10 -n 1 rsync -au c/{} cc/
+cp -f c/ccache.conf cc
+fi
+
+
+
 repo init -u https://github.com/crdroidandroid/android.git -b 14.0 --git-lfs
 rm -rf .repo/local_manifests
 mkdir .repo/local_manifests
@@ -6,8 +29,15 @@ cp scripts/roomservice.xml .repo/local_manifests
 mv scripts/statix.xml .repo/manifests/snippets
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
 chmod +x tsync.sh
-. tsync.sh
-#m clean
-#make clean
+source scripts/tsync.sh
+source build/envsetup.sh
+    lunch lineage_us997-userdebug
+    m installclean
+    m -j$(nproc --all) bacon
+
+
+time ls -1 cc | xargs -I {} -P 10 -n 1 rsync -au cc/{} c/
+cp -f cc/ccache.conf c
+ccache -s
 
 
