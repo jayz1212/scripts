28f267e22f137567a259d365c51ec66fd25b8a9c
jayz1212 <160797500+jayz1212@users.noreply.github.com>
2024-03-19 15:58:54 +0800

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Set default values for device and command
-repo init -u git://github.com/LineageOS/android.git -b lineage-15.0
+repo init -u https://github.com/LineageOS/android.git -b lineage-18.1 --git-lfs
 /opt/crave/resync.sh
