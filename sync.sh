4ff246a1ed6abe3e9e2842537c841cb960621d6b
jayz1212 <160797500+jayz1212@users.noreply.github.com>
2024-03-19 08:29:54 +0800

    test

--- sync.sh
+++ sync.sh
@@ -1,34 +1,91 @@
 #!/bin/bash
+# Set default values for device and command
+DEVICE="${1:-all}"  # If no value is provided, default to "all"
+COMMAND="${2:-build}"  # If no value is provided, default to "build"
+DELZIP="${3}"
 
-repo init -u https://github.com/crdroidandroid/android.git -b 14.0 --git-lfs
+mkdir -p cc
+mkdir -p c
+sudo apt-get update -y
+sudo apt-get install -y apt-utils
+sudo apt-get install -y ccache
+sleep 1
+export USE_CCACHE=1
+sleep 1
+export CCACHE_DIR=$PWD/cc
+sleep 1 
+ccache -M 100G
+ccache -s
+ccache --set-config=compression=false
+ccache --show-config | grep compression
+echo $CCACHE_DIR
+ccache -s
 
+if [ -z "$(ls -A c)" ]; then
+  echo "Folder c is empty. Skipping the rsync command."
+else
+  # If folder c is not empty, execute the rsync command
+time ls -1 c | xargs -I {} -P 10 -n 1 rsync -au c/{} cc/
+cp -f c/ccache.conf cc
+fi
 
+## Remove existing build artifactsa
+if [ "$DELZIP" == "delzip" ]; then
+    rm -rf out/target/product/*/*.zip
+fi
+
+#git clean -fdX
+#rm -rf frameworks/base/
+rm -rf .repo/local_manifests
+#rm -rf device/lge/
+#rm -rf kernel/lge/msm8996
+mkdir -p .repo/local_manifests
+cp scripts/roomservice.xml .repo/local_manifests
+source scripts/clean.sh
 /opt/crave/resync.sh
-# # Sync repositories and capture the output
-# output=$(repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags 2>&1)
 
-# # Check if there are any failing repositories
-# if echo "$output" | grep -q "Failing repos:"; then
-#     echo "Deleting failing repositories..."
-#     # Extract failing repositories from the error message and echo the deletion path
-#     while IFS= read -r line; do
-#         # Extract repository name and path from the error message
-#         repo_info=$(echo "$line" | awk -F': ' '{print $NF}')
-#         repo_path=$(dirname "$repo_info")
-#         repo_name=$(basename "$repo_info")
-#         # Echo the deletion path
-#         echo "Deleted repository: $repo_info" 
-#         # Save the deletion path to a text file
-#         echo "Deleted repository: $repo_info" >> deleted_repositories.txt
-#         # Delete the repository
-#         rm -rf "$repo_path/$repo_name"
-#     done <<< "$(echo "$output" | awk '/Failing repos:/ {flag=1; next} /Try/ {flag=0} flag')"
-
-#     # Re-sync all repositories after deletion
-#     echo "Re-syncing all repositories..."
-#     repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
-# else
-#     echo "All repositories synchronized successfully."
-# fi
+#wget -O a.py https://raw.githubusercontent.com/xc112lg/crdroid10.1/main/a.py
+#chmod +x a.py
+chmod +x scripts/export.sh
+export USE_CCACHE=1
+source scripts/fixes.sh
+export USE_CCACHE=1
+source build/envsetup.sh
 
 
+# Check if command is "clean"
+if [ "$COMMAND" == "clean" ]; then
+    echo "Cleaning..."
+    m clean
+fi
+
+# Check if device is set to "all"
+if [ "$DEVICE" == "all" ]; then
+    echo "Building for all devices..."
+
+    lunch lineage_us997-userdebug
+    m installclean
+    m -j$(nproc --all) bacon
+    lunch lineage_h870-userdebug
+    m installclean
+    m -j$(nproc --all) bacon
+    lunch lineage_h872-userdebug
+    m installclean
+    m -j$(nproc --all) bacon
+ 
+elif [ "$DEVICE" == "h872" ]; then
+    echo "Building for h872..."
+export BUILD_DEVICE="h872"
+    lunch lineage_h872-userdebug
+    m installclean
+    m -j$(nproc --all) bacon
+else
+    echo "Building for the specified device: $DEVICE..."
+    # Build for the specified device
+    lunch "$DEVICE"
+    m -j16 bacon
+fi
+
+time ls -1 cc | xargs -I {} -P 10 -n 1 rsync -au cc/{} c/
+cp -f cc/ccache.conf c
+ccache -s
