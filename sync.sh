dd3e6990fe98ece0944d5d1c0cba5fad9f3ecd72
jayz1212 <160797500+jayz1212@users.noreply.github.com>
2024-03-21 11:15:58 +0800

    g

--- sync.sh
+++ sync.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Set default values for device and command
+
 DEVICE="${1:-all}"  
 COMMAND="${2:-build}" 
 DELZIP="${3}"
@@ -10,13 +10,15 @@ COM2="${7}"
 CORE="${8:-"$(nproc --all)"}"
 mkdir -p cc
 mkdir -p c
-sudo apt update -y
-sudo apt install -y apt-utils
-sudo apt upgrade -y ccache
-sudo /usr/sbin/update-ccache-symlinks
-echo 'export PATH="/usr/local/bin:$PATH"' >> ~/.bashrc
-sleep 1
-echo $PATH
+# Set default values for device and command
+wget https://github.com/ccache/ccache/releases/download/v4.9.1/ccache-4.9.1-linux-x86_64.tar.xz
+tar -xf ccache-4.9.1-linux-x86_64.tar.xz
+cd ccache-4.9.1-linux-x86_64
+sudo cp ccache /usr/local/bin/
+ls /usr/local/bin/
+sudo ln -s ccache /usr/local/bin/gcc
+sudo ln -s ccache /usr/local/bin/g++
+cd ..
 export USE_CCACHE=1
 sleep 1
 export CCACHE_DIR=$PWD/cc
@@ -26,6 +28,7 @@ ccache -F 0
 ccache -M 0
 echo $CCACHE_DIR
 ccache -s
+
 
 if [ -z "$(ls -A c)" ]; then
   echo "Folder c is empty. Skipping the rsync command."
@@ -124,3 +127,6 @@ fi
 time ls -1 cc | xargs -I {} -P 10 -n 1 rsync -au cc/{} c/
 cp -f cc/ccache.conf c
 ccache -s
+
+
+
