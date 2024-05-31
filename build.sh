e9f44f5d907619739b6014c1018f9af0b70e8d6f
jayz1212 <160797500+jayz1212@users.noreply.github.com>
2024-05-31 15:41:49 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -88,7 +88,9 @@ cd device/lge/msm8996-common
 git fetch https://github.com/LineageOS/android_device_lge_msm8996-common.git lineage-21
 git cherry-pick 202b7b2a91cd8cf3816f93f90942f1fdf5ebb92f
 sed -i 's/$(call inherit-product, $(SRC_TARGET_DIR)\/product\/non_ab_device\.mk)//g' msm8996.mk
-
+sed -i '/BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true/a\BUILD_BROKEN_INCORRECT_PARTITION_IMAGES := true' BoardConfigCommon.mk
+# git fetch https://github.com/LineageOS/android_device_lge_msm8996-common.git lineage-21
+# git cherry-pick bc3eabcf8adca63c5bd17d61a3066b5d40bdf72f
 cd ../../../
 
 
