3b982261e536e0b8ad0616d03a24bf84f9ad47b7
jayz1212 <160797500+jayz1212@users.noreply.github.com>
2024-06-01 09:15:39 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -40,11 +40,7 @@ m installclean
 m bacon
 
 
-
-
-
-
-rm -rf .repo/local_manifests device/lge/msm8996-common build/tools
+rm -rf .repo/local_manifests device/lge/msm8996-common
 
 mkdir -p .repo/local_manifests
 cp scripts/roomservice.xml .repo/local_manifests
@@ -69,12 +65,64 @@ sed -i '/BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true/a\BUILD_BROKEN_INC
 cd ../../../
 
 cd build/tools
-git fetch https://github.com/xc112lg/android_build.git patch-2
-git cherry-pick c00e85dc1de9217628cdb166af48e13addae360e
+git fetch https://github.com/xc112lg/android_build.git patch-3
+git cherry-pick 1fa6776d7e9287460ef91db066b43636fac60dfb
 cd ../../
 
 
 
 lunch lineage_us997-userdebug
 m installclean
+m bacon
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+rm -rf .repo/local_manifests device/lge/msm8996-common
+
+mkdir -p .repo/local_manifests
+cp scripts/roomservice.xml .repo/local_manifests
+
+
+repo init --git-lfs
+rm -rf external/chromium-webview/prebuilt/*
+rm -rf .repo/projects/external/chromium-webview/prebuilt/*.git
+rm -rf .repo/project-objects/LineageOS/android_external_chromium-webview_prebuilt_*.git
+
+/opt/crave/resync.sh
+
+
+source build/envsetup.sh
+cd device/lge/msm8996-common
+# git fetch https://github.com/LineageOS/android_device_lge_msm8996-common.git lineage-21
+# git cherry-pick 202b7b2a91cd8cf3816f93f90942f1fdf5ebb92f
+# sed -i 's/$(call inherit-product, $(SRC_TARGET_DIR)\/product\/non_ab_device\.mk)//g' msm8996.mk
+sed -i '/BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true/a\BUILD_BROKEN_INCORRECT_PARTITION_IMAGES := true' BoardConfigCommon.mk
+# git fetch https://github.com/LineageOS/android_device_lge_msm8996-common.git lineage-21
+# git cherry-pick bc3eabcf8adca63c5bd17d61a3066b5d40bdf72f
+cd ../../../
+
+
+cd build/tools
+git fetch https://github.com/xc112lg/android_build.git patch-1
+git cherry-pick 694e43d946ee01e61b0ea0d705e5537a616d7c89
+cd ../../
+
+
+cp -r out/target/product/us997/recovery.img out/target/product/us997/obj/PACKAGING/target_files_intermediates/lineage_us997-target_files-eng.admin/SYSTEM/vendor/etc/
+
+
+
+lunch lineage_us997-userdebug
+#m installclean
 m bacon
