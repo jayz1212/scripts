e134abfd4844a797adbdba6420021500dc428cf4
jayz1212 <160797500+jayz1212@users.noreply.github.com>
2024-06-02 12:58:52 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,81 +4,6 @@ repo init -u https://github.com/LineageOS/android.git -b lineage-20.0 --git-lfs
 git lfs install
 
 
-rm -rf .repo/local_manifests device/lge/msm8996-common build/tools vendor/lineage-priv
-mkdir -p .repo/local_manifests
-cp scripts/roomservice.xml .repo/local_manifests
-
-
-repo init --git-lfs
-rm -rf external/chromium-webview/prebuilt/*
-rm -rf .repo/projects/external/chromium-webview/prebuilt/*.git
-rm -rf .repo/project-objects/LineageOS/android_external_chromium-webview_prebuilt_*.git
-
-/opt/crave/resync.sh
-
-
-source build/envsetup.sh
-cd device/lge/msm8996-common
-# git fetch https://github.com/LineageOS/android_device_lge_msm8996-common.git lineage-21
-# git cherry-pick 202b7b2a91cd8cf3816f93f90942f1fdf5ebb92f
-# sed -i 's/$(call inherit-product, $(SRC_TARGET_DIR)\/product\/non_ab_device\.mk)//g' msm8996.mk
-sed -i '/BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true/a\BUILD_BROKEN_INCORRECT_PARTITION_IMAGES := true' BoardConfigCommon.mk
-# git fetch https://github.com/LineageOS/android_device_lge_msm8996-common.git lineage-21
-# git cherry-pick bc3eabcf8adca63c5bd17d61a3066b5d40bdf72f
-cd ../../../
-
-cd build/tools
-git fetch https://github.com/xc112lg/android_build.git patch-1
-git cherry-pick b7b12b875a97eee6e512c74c53a82066e237a31a
-cd ../../
-
-
-
-
-lunch lineage_us997-userdebug
-m installclean
-m bacon
-
-rm -rf .repo/local_manifests device/lge/msm8996-common build/tools vendor/lineage-priv
-mkdir -p .repo/local_manifests
-cp scripts/roomservice.xml .repo/local_manifests
-
-
-repo init --git-lfs
-rm -rf external/chromium-webview/prebuilt/*
-rm -rf .repo/projects/external/chromium-webview/prebuilt/*.git
-rm -rf .repo/project-objects/LineageOS/android_external_chromium-webview_prebuilt_*.git
-
-/opt/crave/resync.sh
-
-
-source build/envsetup.sh
-cd device/lge/msm8996-common
-# git fetch https://github.com/LineageOS/android_device_lge_msm8996-common.git lineage-21
-# git cherry-pick 202b7b2a91cd8cf3816f93f90942f1fdf5ebb92f
-# sed -i 's/$(call inherit-product, $(SRC_TARGET_DIR)\/product\/non_ab_device\.mk)//g' msm8996.mk
-# git fetch https://github.com/LineageOS/android_device_lge_msm8996-common.git lineage-21
-# git cherry-pick bc3eabcf8adca63c5bd17d61a3066b5d40bdf72f
-cd ../../../
-
-cd build/tools
-git fetch https://github.com/xc112lg/android_build.git patch-1
-git cherry-pick b7b12b875a97eee6e512c74c53a82066e237a31a
-cd ../../
-
-
-
-
-lunch lineage_us997-userdebug
-m installclean
-m bacon
-
-
-
-
-
-
-
 rm -rf .repo/local_manifests device/lge/msm8996-common build/tools
 
 mkdir -p .repo/local_manifests
@@ -94,14 +19,6 @@ rm -rf .repo/project-objects/LineageOS/android_external_chromium-webview_prebuil
 
 
 source build/envsetup.sh
-cd device/lge/msm8996-common
-# git fetch https://github.com/LineageOS/android_device_lge_msm8996-common.git lineage-21
-# git cherry-pick 202b7b2a91cd8cf3816f93f90942f1fdf5ebb92f
-# sed -i 's/$(call inherit-product, $(SRC_TARGET_DIR)\/product\/non_ab_device\.mk)//g' msm8996.mk
-sed -i '/BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true/a\BUILD_BROKEN_INCORRECT_PARTITION_IMAGES := true' BoardConfigCommon.mk
-# git fetch https://github.com/LineageOS/android_device_lge_msm8996-common.git lineage-21
-# git cherry-pick bc3eabcf8adca63c5bd17d61a3066b5d40bdf72f
-cd ../../../
 
 cd build/tools
 git fetch https://github.com/xc112lg/android_build.git patch-1
@@ -168,65 +85,6 @@ cd ../../../
 cd build/tools
 git fetch https://github.com/xc112lg/android_build.git patch-1
 git cherry-pick b7b12b875a97eee6e512c74c53a82066e237a31a
-git fetch https://github.com/xc112lg/android_build.git patch-3
-git cherry-pick 78d9accb380bb9b171102a8a135ac8970ba10ab6
-cd ../../
-
-subject='/C=DE/ST=Germany/L=Berlin/O=Max Mustermann/OU=Max Mustermann/CN=Max Mustermann/emailAddress=max@mustermann.de'
-mkdir ~/.android-certs
-
-for x in releasekey platform shared media networkstack testkey cyngn-priv-app bluetooth sdk_sandbox verifiedboot; do \
- yes "" |   ./development/tools/make_key ~/.android-certs/$x "$subject"; \
-done
-
-mkdir vendor/extra
-mkdir vendor/lineage-priv
-cp -r ~/.android-certs vendor/extra/keys
-
-cp -r ~/.android-certs vendor/lineage-priv/keys
-echo "PRODUCT_DEFAULT_DEV_CERTIFICATE := vendor/lineage-priv/keys/releasekey" > vendor/lineage-priv/keys/keys.mk
-cat << 'EOF' >  vendor/lineage-priv/keys/BUILD.bazel
-filegroup(
-    name = "android_certificate_directory",
-    srcs = glob([
-        "*.pk8",
-        "*.pem",
-    ]),
-    visibility = ["//visibility:public"],
-)
-EOF
-
-
-
-rm -rf .repo/local_manifests device/lge/msm8996-common build/tools
-
-mkdir -p .repo/local_manifests
-cp scripts/roomservice.xml .repo/local_manifests
-
-
-repo init --git-lfs
-rm -rf external/chromium-webview/prebuilt/*
-rm -rf .repo/projects/external/chromium-webview/prebuilt/*.git
-rm -rf .repo/project-objects/LineageOS/android_external_chromium-webview_prebuilt_*.git
-
-/opt/crave/resync.sh
-
-
-source build/envsetup.sh
-cd device/lge/msm8996-common
-# git fetch https://github.com/LineageOS/android_device_lge_msm8996-common.git lineage-21
-# git cherry-pick 202b7b2a91cd8cf3816f93f90942f1fdf5ebb92f
-# sed -i 's/$(call inherit-product, $(SRC_TARGET_DIR)\/product\/non_ab_device\.mk)//g' msm8996.mk
-sed -i '/BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true/a\BUILD_BROKEN_INCORRECT_PARTITION_IMAGES := true' BoardConfigCommon.mk
-# git fetch https://github.com/LineageOS/android_device_lge_msm8996-common.git lineage-21
-# git cherry-pick bc3eabcf8adca63c5bd17d61a3066b5d40bdf72f
-cd ../../../
-
-cd build/tools
-git fetch https://github.com/xc112lg/android_build.git patch-1
-git cherry-pick b7b12b875a97eee6e512c74c53a82066e237a31a
-git fetch https://github.com/xc112lg/android_build.git patch-3
-git cherry-pick 78d9accb380bb9b171102a8a135ac8970ba10ab6
 cd ../../
 
 subject='/C=DE/ST=Germany/L=Berlin/O=Max Mustermann/OU=Max Mustermann/CN=Max Mustermann/emailAddress=max@mustermann.de'
