fcc35f142c6198f9e6be5ef5d3b3af20326ecda5
jayz1212 <160797500+jayz1212@users.noreply.github.com>
2024-06-18 11:35:48 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -48,7 +48,7 @@ main $*
 
 #chmod +x scripts/generate_certs.sh
 #source scripts/generate_certs.sh
-
+git clone https://gitlab.com/MindTheGapps/vendor_gapps -b tau vendor/gapps
 ls ./.android-certs/
 
 
@@ -76,95 +76,7 @@ cd ../../
 
 
 
-
-#rm -rf ~/.android-certs vendor/extra/keys
-
-
-#echo "PRODUCT_DEFAULT_DEV_CERTIFICATE := vendor/extra/keys/releasekey" > vendor/extra/product.mk
-# cat << 'EOF' >  vendor/lineage-priv/keys/BUILD.bazel
-# filegroup(
-#     name = "android_certificate_directory",
-#     srcs = glob([
-#         "*.pk8",
-#         "*.pem",
-#     ]),
-#     visibility = ["//visibility:public"],
-# )
-# EOF
-
-
-
-
-
-#lunch lineage_us997-userdebug
-# lunch lineage_h872-userdebug
-# m installclean
-# m bacon
-
-# rm -rf .repo/local_manifests device/lge/msm8996-common build/tools
-
-# mkdir -p .repo/local_manifests
-# cp scripts/roomservice.xml .repo/local_manifests
-
-
-# repo init --git-lfs
-# rm -rf external/chromium-webview/prebuilt/*
-# rm -rf .repo/projects/external/chromium-webview/prebuilt/*.git
-# rm -rf .repo/project-objects/LineageOS/android_external_chromium-webview_prebuilt_*.git
-
-# /opt/crave/resync.sh
-
-
-# source build/envsetup.sh
-# cd device/lge/msm8996-common
-# # git fetch https://github.com/LineageOS/android_device_lge_msm8996-common.git lineage-21
-# # git cherry-pick 202b7b2a91cd8cf3816f93f90942f1fdf5ebb92f
-# # sed -i 's/$(call inherit-product, $(SRC_TARGET_DIR)\/product\/non_ab_device\.mk)//g' msm8996.mk
-# sed -i '/BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true/a\BUILD_BROKEN_INCORRECT_PARTITION_IMAGES := true' BoardConfigCommon.mk
-
-# # git fetch https://github.com/LineageOS/android_device_lge_msm8996-common.git lineage-21
-# # git cherry-pick bc3eabcf8adca63c5bd17d61a3066b5d40bdf72f
-# cd ../../../
-# sed -i '/-include device\/lge\/msm8996-common\/BoardConfigCommon.mk/a\-include vendor/lineage-priv/keys/keys.mk' device/lge/g6-common/BoardConfigCommon.mk
-# cd build/tools
-# git fetch https://github.com/xc112lg/android_build.git patch-1
-# git cherry-pick b7b12b875a97eee6e512c74c53a82066e237a31a
-# cd ../../
-
-# subject='/C=DE/ST=Germany/L=Berlin/O=Max Mustermann/OU=Max Mustermann/CN=Max Mustermann/emailAddress=max@mustermann.de'
-# mkdir ~/.android-certs
-
-# for x in releasekey platform shared media networkstack testkey cyngn-priv-app bluetooth sdk_sandbox verifiedboot; do \
-#  yes "" |   ./development/tools/make_key ~/.android-certs/$x "$subject"; \
-# done
-#sed -i '/-include device\/lge\/msm8996-common\/BoardConfigCommon.mk/a\-include vendor/lineage-priv/keys/keys.mk' device/lge/g6-common/BoardConfigCommon.mk
-
-
-# subject='/C=PH/ST=Metro Manila/L=Manila/O=RexC/OU=RexC/CN=Rexc/emailAddress=dtiven13@gmail.com'
-# mkdir ~/.android-certs
-
-# for x in releasekey platform shared media networkstack testkey bluetooth sdk_sandbox verifiedboot; do \
-#  yes "" |   ./development/tools/make_key ~/.android-certs/$x "$subject"; \
-# done
-
-
-
-# sed -i '/include $(LOCAL_PATH)\/vendor_prop.mk/a -include vendor/lineage-priv/keys/keys.mk' device/lge/msm8996-common/msm8996.mk
-# mkdir vendor/lineage-priv
-# mv ~/.android-certs vendor/lineage-priv/keys
-# echo "PRODUCT_DEFAULT_DEV_CERTIFICATE := vendor/lineage-priv/keys/releasekey" > vendor/lineage-priv/keys/keys.mk
-# cat <<EOF > vendor/lineage-priv/keys/BUILD.bazel
-# filegroup(
-#     name = "android_certificate_directory",
-#     srcs = glob([
-#         "*.pk8",
-#         "*.pem",
-#     ]),
-#     visibility = ["//visibility:public"],
-# )
-# EOF
-
-
+sed -i '/include $(LOCAL_PATH)\/vendor_prop.mk/a include vendor/gapps/arm64/arm64-vendor.mk' device/lge/msm8996-common/msm8996.mk
 sed -i 's/183621644ce05/183181402cc4c/g' device/lge/h872/lineage_h872.mk
 cat device/lge/h872/lineage_h872.mk
 
